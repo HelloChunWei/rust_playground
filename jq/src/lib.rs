d8d92d57d0fb72a4f8278239f19af228be83ea22
Neil Kuo <kissme3131@gmail.com>
2024-06-01 19:03:57 +0800

    refactor: create `parse_command` to parse command easily

--- jq/src/lib.rs
+++ jq/src/lib.rs
@@ -1,3 +1,8 @@
+
+pub struct Command {
+    pub array_operation: bool,
+    pub filter_operation: Option<String>,
+}
 pub fn match_user<'a>(key: String, name: &str, data: &'a serde_json::Value) -> Result<&'a serde_json::Value, anyhow::Error> {
     for data in data.as_array().unwrap() {
         if data[key.clone()] == name {
@@ -5,4 +10,31 @@ pub fn match_user<'a>(key: String, name: &str, data: &'a serde_json::Value) -> R
         }
     }
     Err(anyhow::anyhow!("not found"))
+}
+pub fn parse_command(command_str: &str) -> Command {
+    let  array_operation = false;
+    let mut filter_operation = None;
+
+    // if first_commmand is not ".[]", throw error
+    if !command_str.starts_with(".[]") {
+        let err = anyhow::anyhow!("first command must be .[]");
+        eprintln!("ERROR: {}", err);
+        std::process::exit(1);
+    } 
+
+    // 解析 filter 操作
+    if let Some(filter_str) = command_str.strip_prefix(".[] | select(") {
+        if filter_str.ends_with(")") {
+            filter_operation = Some(filter_str[..filter_str.len() - 1].to_string());
+        }
+    } else {
+        let err = anyhow::anyhow!("second command must be select()");
+        eprintln!("ERROR: {}", err);
+        std::process::exit(1);
+    }
+
+    Command {
+        array_operation,
+        filter_operation,
+    }
 }
--- jq/src/main.rs
+++ jq/src/main.rs
@@ -8,7 +8,6 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-
 // example cargo run -- ".[] | select(.name == \"foo\")" users.json
 
 // users.json
@@ -33,48 +32,32 @@ fn main() ->Result<(), Box<dyn std::error::Error>> {
     println!("Command: {}", args.command);
     println!("Path: {}", args.path.display());
     // parse command and get .[] , select(.name == "foo")
-    // just only can split by string?
-    let mut command = args.command.split("|");
-
-    // get fisrt command like: .[] replace " and space
-    let first_command = command.next().unwrap().replace("\"","").replace(" ", "");
-    // get second command like: select(.name == "foo") replace " and space
-    let second_command = command.next().unwrap().replace("\"","").replace(" ", "");
-    // if first_commmand is not ".[]", throw error
-    if first_command != ".[]" {
-        let err = anyhow::anyhow!("first command must be .[]");
-        eprintln!("ERROR: {}", err);
-        std::process::exit(1);
-    }
-    // if second_command is not match pattern "select()", throw error
-    if !second_command.starts_with("select(") || !second_command.ends_with(")") {
-        let err = anyhow::anyhow!("second command must be select()");
-        eprintln!("ERROR: {}", err);
-        std::process::exit(1);
-    }
-    // find key and value in select()
-    let key_value = second_command.replace("select(", "").replace(")", "");
-    let key_value: Vec<&str> = key_value.split("==").collect();
-    let key = key_value[0].replace(".", "");
-    let value = key_value[1];
-    println!("key: {}", key);
-    println!("value: {}", value);
-
-    // read file
-    let content = std::fs::read_to_string(&args.path)
+    let command = jq::parse_command(&args.command);
+    
+    if let Some(filter) = &command.filter_operation {
+        let key_value: Vec<&str> = filter.split("==").collect();
+        let key = key_value[0].replace(".", "").replace(" ", "");
+        let value = key_value[1].replace(" ", "");
+        println!("key: {}", key);
+        println!("value: {}", value);
+        // read file
+        let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
-    // parse json
-    let json: serde_json::Value = serde_json::from_str(&content)?;
-    let user: &Value = jq::match_user(key, &value ,&json)?;
-    // pretty print
-    // like
-    // {
-    //     "name": "foo",
-    //     "age": 20
-    // }
-    println!("{}", serde_json::to_string_pretty(user)?);
-
-
+        // parse json
+        let json: serde_json::Value = serde_json::from_str(&content)?;
+        let user: &Value = jq::match_user(key, &value ,&json)?;
+        // pretty print
+        // like
+        // {
+        //     "name": "foo",
+        //     "age": 20
+        // }
+        println!("{}", serde_json::to_string_pretty(user)?);
+    } else {
+        let err = anyhow::anyhow!("PARSE COMMAND FAILED");
+        eprintln!("ERROR: {}", err);
+        std::process::exit(1);
+    }
     Ok(())
 }
 
